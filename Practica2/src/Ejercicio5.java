daca46142383d0477bdef59d2fd726c13f5e4275
e-Luque <CrisLuque46@gmail.com>
2025-10-06 12:49:28 +0200

    Ejercicio5.java creado y finalizado

---
+++ Practica2/src/Ejercicio5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double suma = 0;
+
+        System.out.print("Introduce el tamaño del array: ");
+        int tamaño = scanner.nextInt();
+        double[] arrayInt = new double[tamaño];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = Math.random() * 9;
+        }
+        //for (int i = 0; i < arrayInt.length; i++) {
+            //System.out.println(arrayInt[i]);
+        //}
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            suma += arrayInt[i];
+        }
+
+
+        System.out.println("La suma es: " + suma);
+        scanner.close();
+    }
+}
