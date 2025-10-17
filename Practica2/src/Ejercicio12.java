1c9da3996d0531e67d9c7969d68de95b04e8f732
e-Luque <CrisLuque46@gmail.com>
2025-10-17 10:54:18 +0200

    Ejercicio12 creado y finalizado, pero mal terminado

--- Practica2/src/Ejercicio12.java
+++ Practica2/src/Ejercicio12.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Ejercicio12 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] Letras = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",};
+        System.out.println("Introduzca la numeracion de su DNI: ");
+        int DNI = scanner.nextInt();
+        int LetraDNI = DNI%23;
+        System.out.println("Su DNI al completo es: "+DNI+Letras[LetraDNI]);
+    }
 }
