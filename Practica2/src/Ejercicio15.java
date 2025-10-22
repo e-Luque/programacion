87f44a3e845bbd316bda0b9e8ce963f559f9fd50
e-Luque <CrisLuque46@gmail.com>
2025-10-22 12:53:17 +0200

    Ejercicio15.java finalizado

--- Practica2/src/Ejercicio15.java
+++ Practica2/src/Ejercicio15.java
@@ -5,10 +5,19 @@ public class Ejercicio15 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> Numeros = new ArrayList<>(10);
-        for (int i = 0; i< Numeros.size(); i++){
+        for (int i = 0; i< 10; i++){
+            System.out.println("Introduce el Nº "+(i+1)+" de la lista");
             int x = scanner.nextInt();
             Numeros.add(x);
         }
-        //System.out.println(Numeros.toString());
+        for (int i = 0; i < Numeros.size(); i++) {
+            for (int j = 1; j < Numeros.size(); j++) {
+                if (Numeros.get(i).equals(Numeros.get(j)) && i != j){
+                    Numeros.remove(j);
+                    j--;
+                }
+            }
+        }
+        System.out.println("La lista de numeros es: "+ Numeros.toString());
     }
 }
