98093e4f9eaa6da419ce632a1da7d9c089ceb9b3
e-Luque <CrisLuque46@gmail.com>
2025-10-24 08:52:13 +0200

    Ejercicio12.java corregido

--- Practica2/src/Ejercicio12.java
+++ Practica2/src/Ejercicio12.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ejercicio12 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        String[] Letras = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",};
+        String[] Letras = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
         System.out.println("Introduzca la numeracion de su DNI: ");
         int DNI = scanner.nextInt();
         int LetraDNI = DNI%23;
