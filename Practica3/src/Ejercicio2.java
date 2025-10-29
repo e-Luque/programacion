730583870814fd7534060c3be1ce051843710cb0
e-Luque <CrisLuque46@gmail.com>
2025-10-29 14:28:21 +0100

    Ejercicio2.java creado pero no finalizado

---
+++ Practica3/src/Ejercicio2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int[][] Tabla = new int [4][5];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < Tabla.length; i++) {
+            for (int j = 0; j < Tabla[i].length; j++) {
+                Tabla[i][j] = scanner.nextInt();
+
+            }
+        }
+        int SumaI = 0;
+        int valor = 0;
+        for (int i = 0; i < Tabla.length; i++) {
+            for (int j = 0; j < Tabla[i].length; j++) {
+                System.out.printf("%-10s", Tabla[i][j]);
+                SumaI+=Tabla[i][j];
+            }
+            System.out.printf("%-10s", SumaI);
+            System.out.println();
+        }
+        for (int j = 0; j < Tabla[0].length; j++) {
+            int SumaJ = 0;
+            for (int i = 0; i < Tabla.length; i++) {
+                SumaJ += Tabla[i][j];
+            }
+            System.out.printf("%-10s",SumaJ);
+            valor = SumaJ;
+        }
+        System.out.printf("%-10s", (valor+SumaI));
+
+    }
+}
