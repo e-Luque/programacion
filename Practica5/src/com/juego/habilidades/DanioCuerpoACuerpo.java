3b178580113a6aa128f2bf7c5cbff921c7219110
e-Luque <CrisLuque46@gmail.com>
2026-01-14 14:26:01 +0100

    Cambios grandes, voy a realizar un cambio de estructura en Habilidades

--- Practica5/src/com/juego/habilidades/DanioCuerpoACuerpo.java
+++ Practica5/src/com/juego/habilidades/DanioCuerpoACuerpo.java
@@ -1,9 +1,27 @@
 package com.juego.habilidades;
 
-public class DanioCuerpoACuerpo extends HabilidadBase {
+public class DanioCuerpoACuerpo implements Habilidades {
+    private String nombre;
+    private int usosMaximos;
+    private int usosRestantes;
+    private int valor;
 
     public DanioCuerpoACuerpo() {
-        super("Golpe cuerpo a cuerpo", 5, 10);
+    }
+
+    @Override
+    public String getNombre() {
+        return "";
+    }
+
+    @Override
+    public int getUsosRestantes() {
+        return 0;
+    }
+
+    @Override
+    public boolean sePuedeUsar() {
+        return false;
     }
 
     @Override
