ceb4661b6a498fca2bd0ca2e8a4f9dc9bb431eff
e-Luque <CrisLuque46@gmail.com>
2026-02-16 14:14:28 +0100

    Finalizacion del proyecto

--- Practica-Entornos/src/Main.java
+++ Practica-Entornos/src/Main.java
@@ -1,6 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        Oyentes luque = new Oyentes("e-Luque");
-        Artistas cristian = new Artistas("Cristian",954046);
     }
 }
